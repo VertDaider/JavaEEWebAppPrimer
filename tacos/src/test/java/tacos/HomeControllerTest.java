99864492f839f494e57be5e78d86c4270ceade56
VertDaider <serioussem92@gmail.com>
2021-04-22 20:58:28 +0700

    added validation, end ch2

--- tacos/src/test/java/tacos/HomeControllerTest.java
+++ tacos/src/test/java/tacos/HomeControllerTest.java
@@ -12,9 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.web.WebConfig;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)   // <1>
 public class HomeControllerTest {
 
   @Autowired
